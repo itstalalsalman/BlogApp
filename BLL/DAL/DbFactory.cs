3afffbce640e21b9473147271810ce6021d0bf53
itstalalsalman <mustangcreator@gmail.com>
2025-01-03 20:43:29 +0300

    Session implemented (the blogs can be liked and through navbar can be checked in favorites).

---
+++ BLL/DAL/DbFactory.cs
@@ -0,0 +1,16 @@
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace BLL.DAL
+{
+    public class DbFactory : IDesignTimeDbContextFactory<Db>
+    {
+        public Db CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<Db>();
+            optionsBuilder.UseSqlServer("server=(localdb)\\mssqllocaldb;database=BlogsDB;trusted_connection=true;");
+            return new Db(optionsBuilder.Options);
+        }
+    }
+}
---
+++ BLL/Models/FavoriteModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+
+namespace BLL.Models
+{
+    public class FavoritesModel
+    {
+        public int BlogId { get; set; }
+        public int UserId { get; set; }
+
+        [DisplayName("Blog Name")]
+        public string BlogName { get; set; }
+    }
+}
